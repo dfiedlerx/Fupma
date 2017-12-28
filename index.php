3087ecbd3c643c1deb115edb5f75df2ecb048204
Daniel Fiedler <daniel.fiedler@mundowap.com.br>
2017-12-28 15:43:36 -0200

    Removendo pasta src

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+//Arquivo que carregará as configurações
+require 'config/config.php';
+
+// Auto Loader que chamara as classes do sistema.
+require CORE_DIRECTORY."autoload.php";
+
+$core = (new core())->run();
