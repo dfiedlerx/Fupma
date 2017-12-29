b2c6794a9d7aa704d9d0f9be6727998ce5dbcf07
Daniel Fiedler <daniel.fiedler@mundowap.com.br>
2017-12-28 22:00:47 -0200

    Change Autoload
    - Added multi-folder Namespace suport

--- core/init/autoload.php
+++ core/init/autoload.php
@@ -10,16 +10,6 @@ spl_autoload_register(function ($className) {
 
     $className = str_replace('\\', '/', $className);
 
-    if (strpos($className, CONTROLLERS_COMPLEMENT)) {
-
-        if (file_exists(CONTROLLERS_DIRECTORY . $className . '.php')) {
-            
-            require (CONTROLLERS_DIRECTORY . $className . '.php');
-
-        }
-        
-    }
-
     if (file_exists(MODELS_DIRECTORY . $className . MODELS_COMPLEMENT . '.php')) {
 
         require MODELS_DIRECTORY . $className . MODELS_COMPLEMENT . '.php';
@@ -29,6 +19,12 @@ spl_autoload_register(function ($className) {
     if (file_exists(CORE_DIRECTORY . $className . CORE_COMPLEMENT . '.php')) {
 
         require CORE_DIRECTORY . $className . CORE_COMPLEMENT . '.php';
+
+    }
+
+    if (file_exists(CONTROLLERS_DIRECTORY . $className . '.php')) {
+
+        require (CONTROLLERS_DIRECTORY . $className . '.php');
 
     }
 
