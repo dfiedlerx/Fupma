3af4bd3f0eda6bb2c266fe799e30b94869e2f858
Daniel Fiedler <daniel.fiedler@mundowap.com.br>
2018-01-03 11:33:21 -0200

    Added isACorrectStringLenght method

--- models/ModelTools/VarContentValidator.Class.php
+++ models/ModelTools/VarContentValidator.Class.php
@@ -13,73 +13,82 @@ class VarContentValidator
 {
 
     //Método que apenas filtra uma variavel com um filtro fixo ou personalizado
-    public static function internalFilter (string $stringToFilter, int $typeOfFilter = FILTER_SANITIZE_STRING) {
+    public static function internalFilter (string $stringToFilter, int $typeOfFilter = FILTER_SANITIZE_STRING) :boll {
 
         return filter_var ($stringToFilter, $typeOfFilter);
 
     }
 
     //Método responsável por retornar se determinado conteudo e do tipo email
-    public static function isAValidEmail (string $contentToValidate) {
+    public static function isAValidEmail (string $contentToValidate) :boll {
 
         return filter_var ($contentToValidate, FILTER_VALIDATE_EMAIL);
 
     }
 
     //Verifica se o conteudo e do tipo inteiro
-    public static function isAValidInt (string $contentToValidate) {
+    public static function isAValidInt (string $contentToValidate) :boll {
 
         return filter_var ($contentToValidate, FILTER_VALIDATE_INT);
 
     }
 
     //Verifica se o conteudo é uma url
-    public static function isAValidURL (string $contentToValidate) {
+    public static function isAValidURL (string $contentToValidate) :boll {
 
         return filter_var ($contentToValidate, FILTER_VALIDATE_URL);
 
     }
 
     //Verifica se o conteudo é um endereço IP
-    public static function isAValidIPAddress (string $contentToValidate) {
+    public static function isAValidIPAddress (string $contentToValidate) :boll {
 
         return filter_var ($contentToValidate, FILTER_VALIDATE_IP);
 
     }
 
     //Verifica se o conteúdo é do tipo max
-    public static function isAValidMacAddress (string $contentToValidate) {
+    public static function isAValidMacAddress (string $contentToValidate) :boll {
 
         return filter_var ($contentToValidate, FILTER_VALIDATE_MAC);
 
     }
 
     //Verifica se o conteudo e um dominio
-    public static function isAValidDomain (string $contentToValidate) {
+    public static function isAValidDomain (string $contentToValidate) :boll {
 
         return filter_var ($contentToValidate, FILTER_VALIDATE_DOMAIN);
 
     }
 
     //Verifica se o conteudo e do tipo boolean
-    public static function isAValidBoolean (string $contentToValidate) {
+    public static function isAValidBoolean (string $contentToValidate) :boll {
 
         return filter_var ($contentToValidate, FILTER_VALIDATE_BOOLEAN);
 
     }
 
     //Veirifica se o conteudo e do tipo Float
-    public static function isAValidFloat (string $contentToValidate) {
+    public static function isAValidFloat (string $contentToValidate) :boll {
 
         return filter_var ($contentToValidate, FILTER_VALIDATE_FLOAT);
 
     }
 
     //Verifica se o conteudo é um Regex válido
-    public static function isAValidRegexExp (string $contentToValidate) {
+    public static function isAValidRegexExp (string $contentToValidate) :bool {
 
         return filter_var ($contentToValidate, FILTER_VALIDATE_REGEXP);
 
     }
+
+    //Verifica se o conteudo é exatamente do tamanho especificado
+    public static function isACorrectStringLenght (string $contentToValidate, int $stringLenght) :bool {
+
+        return strlen ($contentToValidate) == $stringLenght;
+
+    }
+
+
 
 }
