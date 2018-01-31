3e698a77fe2a028ab65d79e6fc78f23bb3610ada
Daniel Fiedler <daniel.fiedler@mundowap.com.br>
2018-01-31 16:46:06 -0200

    new DateTime class in core\ModelTools

---
+++ core/Tools/ModelTools/DateTimeTools.php
@@ -0,0 +1,38 @@
+<?php namespace Tools\ModelTools;
+/**
+ * Class DateTimeTools
+ *
+ * Classe responsável por fazer algumas das funções mais comuns de tratamento de data/hora
+ * para as funções do sistema.
+ */
+
+class DateTimeTools
+{
+
+    /**
+     * @param string $dateToAcress
+     * @param string $timeToAcress
+     * @return string
+     */
+    public static function dateAcress (string $dateToAcress, string $timeToAcress) : string {
+
+        return date ($dateToAcress, strtotime($timeToAcress));
+
+    }
+
+    /**
+     * @param int $seconds
+     * @return string
+     */
+    public static function secondsToHours (int $seconds) : string {
+
+        $hours = floor($seconds / 3600);
+
+        return
+            str_pad($hours, 2, '0', STR_PAD_LEFT) . ':' .
+            str_pad(floor(($seconds - ($hours * 3600)) / 60), 2, '0', STR_PAD_LEFT) . ':' .
+            str_pad( floor($seconds % 60), 2, '0', STR_PAD_LEFT);
+
+    }
+
+}
