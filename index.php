c2f12a2252a8d7a787b15d32ab8d6399379b46c0
Daniel Fiedler <32338795+dfiedlerx@users.noreply.github.com>
2018-04-18 09:04:13 -0300

    CORE DIRECTORY

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_startup_erros',1);
 error_reporting(E_ALL);
 
 //Arquivo que carregará as configurações
-require 'core/init/config.php';
+require CORE_DIRECTORY . 'init/config.php';
 
 // Auto Loader que chamara as classes do sistema.
 require CORE_DIRECTORY . 'init/autoload.php';
